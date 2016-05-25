ea1dfba1a21fc14f39b282c1b20ce55adbd3fc24
jtcode42 <tschetterjordan@gmail.com>
2016-05-25 15:27:32 -0500

    Add main color changing script

--- color-change.lua
+++ color-change.lua
@@ -2,3 +2,9 @@
 --Insert this into a part to randomly change its color!
 
 local sp=script.Parent
+
+while true do
+  local num = math.random(1, 127)
+  sp.BrickColor = BrickColor.new(num)
+  wait(0.2)
+end
