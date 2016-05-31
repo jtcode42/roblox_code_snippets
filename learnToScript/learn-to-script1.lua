0b487d3ab462f311fd71b19fa6aebc70e25d37af
jtcode42 <tschetterjordan@gmail.com>
2016-05-31 14:43:47 -0500

    Add new stuff, tweak a little

--- learnToScript/learn-to-script1.lua
+++ learnToScript/learn-to-script1.lua
@@ -2,9 +2,9 @@
 Hello. If you are looking at this, this is a programming tutorial. If you are too lazy to read, take your tl;dr somewhere else, okay? Okay.
 Anyway, we'll be going deeper and deeper into this scripting stuff. But for now, the basics.
 On Roblox, we program in a language called Lua, specifically version 5.1. The admins mutated it(like everything else) into a language that interacts well with the parts in your game.
-We'll start with a very basic command
+We'll start with a very basic command--]]
 
 print("Hello world!")
 
-The print command outputs that line of text to the output window. Very useful to make sure something's working. And whenever you start a new language you MUST print 'hello world'.
+--[[The print command outputs that line of text to the output window. Very useful to make sure something's working. And whenever you start a new language you MUST print 'hello world'. It's one of those programmer things. Anyway, congrats! You are on your way to becoming a programmer. Keep working and you'll get there
 ]]--
